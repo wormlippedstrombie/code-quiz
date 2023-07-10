a8862556d5c70e3564f49206b08d4650fb92efb3
wormlippedstrombie <tysonleethornton@outlook.com>
2023-07-10 13:42:53 +0930

    added basic questions to quiz via Javascript

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+   // Defines quiz questions and answers
+   var questions = [
+    {
+      question: "What is the correct way to declare a JavaScript variable?",
+      choices: ["var myVariable;", "let myVariable;", "const myVariable;", "All of the above"],
+      correctAnswer: "All of the above"
+    },
+    {
+      question: "Which method is used to add an element at the end of an array in JavaScript?",
+      choices: ["push()", "pop()", "shift()", "unshift()"],
+      correctAnswer: "push()"
+    },
+    {
+      question: "What does CSS stand for?",
+      choices: ["Cascading Style Sheets", "Creative Style Sheets", "Colorful Style Sheets", "Computer Style Sheets"],
+      correctAnswer: "Cascading Style Sheets"
+    },
+    // Add more questions here
+  ];
